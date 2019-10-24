1950191b70eeda976d5621bef564ecd6bd671e8d
Bianca Karitas jensen <5884546@rts-365.dk>
2019-10-24 12:06:49 +0200

    Update

--- cheeseshop/dist/assets/js/get-products.js
+++ cheeseshop/dist/assets/js/get-products.js
@@ -1,6 +1,9 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
+  var params = new URLSearchParams(document.location.search);
+  var categoryName = parseInt(params.get("destination_id"));
+  var mainElement = document.querySelector("#cardTemplate");
   fetch("/data/products.json").then(function (response) {
     return response.json();
   }) //Behøver ikke skrive return, hvis der ikke bruges {}
--- cheeseshop/src/js/get-products.js
+++ cheeseshop/src/js/get-products.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", () =>{
 
+	let params = new URLSearchParams(document.location.search);
+	const categoryName = parseInt (params.get("destination_id"));
+	let mainElement = document.querySelector("#cardTemplate");
+
 	fetch("/data/products.json")
 		.then(response => response.json())//Behøver ikke skrive return, hvis der ikke bruges {}
 		.then(function (data){
-			console.log(data);
-		})
-
+			 console.log(data)
+		});
 });
