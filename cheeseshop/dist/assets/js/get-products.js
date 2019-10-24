0a153ec10186287fe4a193275ddfc739eb604d07
Bianca Karitas jensen <5884546@rts-365.dk>
2019-10-24 14:47:09 +0200

    Update

--- cheeseshop/dist/assets/js/get-products.js
+++ cheeseshop/dist/assets/js/get-products.js
@@ -1,13 +1,31 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  var params = new URLSearchParams(document.location.search);
-  var categoryName = parseInt(params.get("destination_id"));
-  var mainElement = document.querySelector("#cardTemplate");
   fetch("/data/products.json").then(function (response) {
     return response.json();
   }) //Behøver ikke skrive return, hvis der ikke bruges {}
   .then(function (data) {
-    console.log(data);
+    //console.log(data)
+    var cardTemplate = document.getElementById("cardTemplate"); //ID loader direkte fra scoop, altså oploades hurtigere.
+
+    var list = document.getElementsByClassName("cardList")[0]; //getElementsB...... er en Node list.
+
+    data.forEach(function (product) {
+      //I data'en henter den for hvert product.
+      //console.log(product);
+      var clone = cardTemplate.content.cloneNode(true); //Henter alt det inde for cardTemplate. Man bibeholder de orginale tags i html'en derfor skriver man true.
+
+      clone.querySelector("h1").innerText = product.navn; //Fordi det ikke er html man lægger ind men tekst.
+
+      clone.querySelector("p").innerText = product.beskrivelse[0];
+      clone.querySelector("img").src = product.billeder[0]; //[0] fordi det er en array og det er det første element i arrayet der bliver valgt & src fordi det er en billede der skal hentes.
+
+      clone.querySelector(".price").innerText = product.pris;
+      clone.querySelector(".weight").innerText = product.vægt;
+      clone.querySelector(".country").innerText = product.land;
+      clone.querySelector("a").href = "/product/?sku=".concat(product.sku); //laver stivej fra hvert enkelt læs mere link, til hvert enkelt produkt. da stivejen i browseren tilføjer ?sku=xxxxxx, varenr. for hvert produkt.
+
+      list.appendChild(clone);
+    });
   });
 });
--- cheeseshop/src/js/get-products.js
+++ cheeseshop/src/js/get-products.js
@@ -1,12 +1,24 @@
 document.addEventListener("DOMContentLoaded", () =>{
 
-	let params = new URLSearchParams(document.location.search);
-	const categoryName = parseInt (params.get("destination_id"));
-	let mainElement = document.querySelector("#cardTemplate");
-
 	fetch("/data/products.json")
 		.then(response => response.json())//Behøver ikke skrive return, hvis der ikke bruges {}
 		.then(function (data){
-			 console.log(data)
+			//console.log(data)
+			const cardTemplate = document.getElementById("cardTemplate");//ID loader direkte fra scoop, altså oploades hurtigere.
+			const list = document.getElementsByClassName("cardList")[0]; //getElementsB...... er en Node list.
+
+			data.forEach(function(product) {//I data'en henter den for hvert product.
+				//console.log(product);
+				const clone = cardTemplate.content.cloneNode(true); //Henter alt det inde for cardTemplate. Man bibeholder de orginale tags i html'en derfor skriver man true.
+				clone.querySelector("h1").innerText = product.navn;//Fordi det ikke er html man lægger ind men tekst.
+				clone. querySelector("p").innerText = product.beskrivelse[0];
+				clone.querySelector("img").src = product.billeder[0];//[0] fordi det er en array og det er det første element i arrayet der bliver valgt & src fordi det er en billede der skal hentes.
+				clone.querySelector(".price").innerText = product.pris;
+				clone.querySelector(".weight").innerText = product.vægt;
+				clone.querySelector(".country").innerText = product.land;
+				clone.querySelector("a").href =`/product/?sku=${product.sku}`;//laver stivej fra hvert enkelt læs mere link, til hvert enkelt produkt. da stivejen i browseren tilføjer ?sku=xxxxxx, varenr. for hvert produkt.
+
+				list.appendChild(clone);
+			})
 		});
 });
