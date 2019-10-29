435aa0d56d6f4a995f82f3d466045892041acc44
Bianca Karitas jensen <5884546@rts-365.dk>
2019-10-29 10:47:52 +0100

    Update

--- cheeseshop/src/js/fs-get-product.js
+++ cheeseshop/src/js/fs-get-product.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", () =>{
 
 			productShow.querySelector("h1").innerText = doc.data().name;
 			productShow.querySelector(".imageBig").src = `/assets/images/${doc.data().image}`;
+			productShow.querySelector("p").innerText = doc.data().description;
 			productShow.querySelector(".price").innerText = doc.data().price;
 			productShow.querySelector(".country").innerText = doc.data().country;
 			productShow.querySelector(".region").innerText = doc.data().region;
@@ -21,27 +22,3 @@ document.addEventListener("DOMContentLoaded", () =>{
 		}
 	});
 });
-
-/* 	fetch("/data/products.json")
-		.then (response => response.json())
-		.then (function (data){
-			
-			data.forEach(function (product) {
-				const sku = new URL(window.location).searchParams.get("sku");//laver variabel som henter en ny url til den nuværende "sku"=........
-				if(product.sku === sku ) {//Hvis product.sku er lig med sku.
-					
-					const productShow = document.querySelector(".product_mainSection")//laver variabel til ydre box af enkelt product visning.
-
-					productShow.querySelector("h1").innerText = product.navn;
-					productShow.querySelector("p").innerText = product.beskrivelse;
-					productShow.querySelector(".imageBig").src = `/assets/images/${product.billeder[0]}`;
-					productShow.querySelector(".price").innerText = product.pris;
-					productShow.querySelector(".weight").innerText = product.vægt;
-					productShow.querySelector(".country").innerText = product.land;
-					productShow.querySelector(".product_imageSmall1").src = `/assets/images/${product.billeder[1]}`;
-					productShow.querySelector(".product_imageSmall2").src = `/assets/images/${product.billeder[2]}`;
-					productShow.querySelector(".product_imageSmall3").src = `/assets/images/${product.billeder[3]}`;
-
-				}
-			});
-		}); */
